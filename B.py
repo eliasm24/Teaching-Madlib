39655c7389dca3999ca56d726e409ecc2005f52c
eliasm24 <118215463+eliasm24@users.noreply.github.com>
2023-10-02 10:06:49 -0400

    Update B.py

--- B.py
+++ B.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-number_between_1_and_9000 = 0 # assign some number or string at the end of each of these lines
-adjective_1 = ""
-noun_1 = ""
-famous_person = ""
-adjective_2 = ""
-very_large_number = 0
-noun_2 = ""
-adjective_2 = ""
-large_store = ""
+number_between_1_and_9000 = 11 # assign some number or string at the end of each of these lines
+adjective_1 = "mid"
+noun_1 = "Hookah"
+famous_person = "Kanye West"
+adjective_2 = "cakey"
+very_large_number = 893736
+noun_2 = "pepper"
+adjective_2 = "zesty"
+large_store = "Roche Brothers"
