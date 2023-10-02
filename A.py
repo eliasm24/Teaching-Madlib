b2d0e803cad857299db67b150083576647970475
eliasm24 <118215463+eliasm24@users.noreply.github.com>
2023-10-02 10:04:36 -0400

    Update A.py

--- A.py
+++ A.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
-dollar_amount = ""
-adjective_2 = ""
-noun_1 = ""
-large_store = ""
+year_in_the_future = 2100# assign numbers or strings to the end of these versions
+wacky_robot_name = "Sputnik"
+adjective_1 = "goofy"
+violent_verb = "murder"
+tech_company = "Apple"
+dollar_amount = "69"
+adjective_2 = "boof"
+noun_1 = "bear"
+large_store = "Walmart"
